a9752de8842fe03fc574f02eb7f76da7042b3335
RomainKH <romain.khanoyan@gmail.com>
2019-03-05 18:46:26 +0100

    go

--- src/js/Controls.js
+++ src/js/Controls.js
@@ -72,7 +72,7 @@ window.addEventListener('scroll', (_event) => {
 
 
 const imgArray = new Array(img0, img1, img2, img3, img4)
-const descriptionArray = new Array('This was a school project I did which basically was about making a mandala with illustrator about a theme we had to choose. As a fan of the Undertale universe I choosed to do mine on this video game.', 'This Film Poster was also a school project, we had to choose between different movies, I choosed Halloween because John Carpenter is one of the many film director I admire.', 'I did this 404 page in a limited amount of time, I was inspired by matrix, I use the character Neo and the universe of the movie in order to recreate this aspect of being lost as it is the purpose of the 404 page.', 'This was my first creation for the DailyUi I did it as a joke (why ordering barbapapa online ?) and using colours I really like to make the best first impression when publicating this online.', 'This one was inspired by many app already existing, where you can see what is in the sky, and what you are looking at, is it a star ? is it a bird ? or is this something no one has ever seen ? who knows...')
+const descriptionArray = new Array('This was a school project I did which basically was about making a mandala with illustrator about a theme we had to chose. As a fan of the Undertale universe I choosed to do mine on this video game.', 'This Film Poster was also a school project, we had to choose between different movies, I choosed Halloween because John Carpenter is one of the many film director I admire.', 'I did this 404 page in a limited amount of time, I was inspired by matrix, I used the character Neo and the universe of the movie in order to recreate the idea of being lost which is the purpose of the 404 page.', 'This was my first creation for the DailyUi I did it as a joke (why ordering barbapapa online ?) and using colours I really like to make the best first impression when publicating this online.', 'This one was inspired by many app already existing, where you can see what is in the sky, and what you are looking at, is it a star ? is it a bird ? or is this something no one has ever seen ? who knows...')
 const titleArray = new Array('Mandala Undertale', 'Olly Moss Film Poster', 'DailyUi: The 404 page', 'DailyUi: SignUp page', 'DailyUi: App Icon')
 
 article.appendChild(bigImg)
@@ -88,27 +88,25 @@ buttonA.addEventListener('click', () => {
     if(i > 4) {
         i = 0
     }
-    bigImg1.onload = () => {
-        bigImg1.style.display = 'block'
-        bigImg.classList.add('getBigger')
-        imgTitle.classList.remove('pauseAnim')
-        imgDescription.classList.remove('pauseAnim')
-        imgDescription.classList.add('changeText')
-        imgTitle.classList.add('changeText')
-        imgDescription.innerHTML = descriptionArray[i]
-        imgTitle.innerHTML = titleArray[i]
-        bigImg1.classList.add('falseBigImg')
-        setTimeout(function(){
-            bigImg.classList.remove('getBigger')
-            imgDescription.classList.remove('changeText')
-            imgTitle.classList.remove('changeText')
-            bigImg1.classList.remove('falseBigImg')
-            bigImg1.removeAttribute('src')
-            bigImg1.style.display = 'none'
-        }, 380)
-    }
     bigImg1.src = bigImg.src
     bigImg.src = imgArray[i]
+    bigImg1.style.display = 'block'
+    bigImg.classList.add('getBigger')
+    imgTitle.classList.remove('pauseAnim')
+    imgDescription.classList.remove('pauseAnim')
+    imgDescription.classList.add('changeText')
+    imgTitle.classList.add('changeText')
+    imgDescription.innerHTML = descriptionArray[i]
+    imgTitle.innerHTML = titleArray[i]
+    bigImg1.classList.add('falseBigImg')
+    setTimeout(function(){
+        bigImg.classList.remove('getBigger')
+        imgDescription.classList.remove('changeText')
+        imgTitle.classList.remove('changeText')
+        bigImg1.classList.remove('falseBigImg')
+        bigImg1.removeAttribute('src')
+        bigImg1.style.display = 'none'
+    }, 380)
 })
 
 buttonB.addEventListener('click', () => {
